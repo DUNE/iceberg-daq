c97851dc58cb7fa2e8d2ee222f7588f42aa30ea0
Andrew Mogan <amogan@fnal.gov>
2025-09-26 10:39:03 -0500

    Add TLU scripts

---
+++ tlu/tlu-status.sh
@@ -0,0 +1,12 @@
+# 
+# TLU Status for DUNEDAQ
+#
+cd /home/dunecet/dunedaq/fddaq-v4.4.6-a9
+source env.sh
+#
+#cd tlu
+#
+dtsbutler mst BOREAS_TLU_ICEBERG status
+
+dtsbutler hsi BOREAS_TLU_ICEBERG readback
+#
